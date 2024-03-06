967467d5686bdf827e136373981b4567f0825e99
JackFranks23 <Jack.franks23@student.cart.org>
2024-03-06 13:41:51 -0800

    Diego JS

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 document.addEventListener("DOMContentLoaded", function () {
     addEventListener('change', function (eventData){
-        document.getElementById("fedTaxes").innerText = (growthMonthly*.07);
-        document.getElementById("stateTaxes").innerText = (growthMonthly*.062);
-        document.getElementById("socialSec").innerText = (growthMonthly*.0145);
-        document.getElementById("mediCare").innerText = (growthMonthly*.01);
-        document.getElementById("fedTaxes").innerText = (growthMonthly*.07);
-        document.getElementById("fedTaxes").innerText = (growthMonthly*.07);
-        document.getElementById("fedTaxes").innerText = (growthMonthly*.07);
-        document.getElementById("fedTaxes").innerText = (growthMonthly*.07);
+
+        let grossyear = document.getElementById("grossyear").value;
+        //grossyear.innertext = FormData.grossyear
+        let grossmonthly =grossyear/12;  
+        //round to the cent Math.floor(x*100)/100
+        document.getElementById("grossmonthly").innerText = (Math.round(grossmonthly*100)/100);
+        document.getElementById("fedtaxes").innerText = ((Math.round(grossmonthly*100*.12)/100));
+        document.getElementById("statetaxes").innerText = ((Math.round(grossmonthly*100*.07)/100));
+        document.getElementById("socialsec").innerText = ((Math.round(grossmonthly*100*.062)/100));
+        document.getElementById("medicare").innerText = ((Math.round(grossmonthly*100*.0145)/100));
+        document.getElementById("retirement").innerText = ((Math.round(grossmonthly*100*.05)/100));
+        document.getElementById("insurance").innerText = (180);
+        document.getElementById("netmonthly").innerText = (grossmonthlygrossmonthly-(Math.round(grossmonthly*100*.12)/100)-(Math.round(grossmonthly*100*.07)/100)-(Math.round(grossmonthly*100*.062)/100)-(Math.round(grossmonthly*100*.0145)/100)-(Math.round(grossmonthly*100*.05)/100)-180); //use the round here
 
     })
 });
