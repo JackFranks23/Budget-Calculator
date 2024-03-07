972cbbd42a2995a5966d842054a46cb6e3decb68
JackFranks23 <Jack.franks23@student.cart.org>
2024-03-07 14:15:54 -0800

    Attempted to move info from index to budget

--- js/app.js
+++ js/app.js
@@ -1,8 +1,16 @@
+const incomeID = `income`;
+
+if (localStorage.getItem(incomeID) === null) {
+    localStorage.setItem(incomeID, JSON.stringify({ total: 0 }));
+}
+
+let income = JSON.parse(localStorage.getItem(incomeID));
+
 document.addEventListener("DOMContentLoaded", function () {
+    let netMonthly = 0;
     addEventListener('change', function (eventData){
 
         let grossyear = document.getElementById("grossyear").value;
-        //grossyear.innertext = FormData.grossyear
         let grossmonthly =grossyear/12;  
         //round to the cent Math.floor(x*100)/100
         document.getElementById("grossmonthly").innerText = (Math.round(grossmonthly*100)/100);
@@ -13,13 +21,10 @@ document.addEventListener("DOMContentLoaded", function () {
         document.getElementById("retirement").innerText = ((Math.round(grossmonthly*100*.05)/100));
         document.getElementById("insurance").innerText = (180);
         // document.getElementById("netmonthly").innerText = (grossmonthlygrossmonthly-(Math.round(grossmonthly*100*.12)/100)-(Math.round(grossmonthly*100*.07)/100)-(Math.round(grossmonthly*100*.062)/100)-(Math.round(grossmonthly*100*.0145)/100)-(Math.round(grossmonthly*100*.05)/100)-180); //use the round here
-        document.getElementById("netmonthly").innerText = ((Math.round(grossmonthly*100*.3165)/100 + 180))
-        let apex = document.getElementById(`netmonthly`)
-        console.log(apex)
-        localStorage.setItem(netmonthly, apex)
-        let storage = localStorage.getItem(netmonthly)
-        document.getElementById(`monthlyIncome2`).innertext = storage
 
+        document.getElementById("netmonthly").innerText = ((Math.round(grossmonthly*100*.3165)/100 + 180));
+        netMonthly = ((Math.round(grossmonthly*100*.3165)/100 + 180));
 
-    })
+    });
+
 });
